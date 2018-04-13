66194c134b5568e683ae26160da705da624d2f13
Feng Xie <feng.xie@gmail.com>
2018-04-13 12:12:39 -0700

    add header

--- src/core/vgroove_reflect.h
+++ src/core/vgroove_reflect.h
@@ -1,92 +1,138 @@
-#include "reflect.h"
+#include "reflection.h"
 
-class VGrooveReflection : public MicrofacetReflection {
-  public:
-    // MicrofacetReflection Public Methods
-    VGrooveReflection(const Spectrum &R,
-                         MicrofacetDistribution *distribution, Fresnel *fresnel)
-        : MicrofacetReflection(r, distribution, fresnel) {}
-    Spectrum f(const Vector3f &wo, const Vector3f &wi) const;
-    Spectrum Sample_f(const Vector3f &wo, Vector3f *wi, const Point2f &u,
-                      Float *pdf, BxDFType *sampledType) const;
-    Float Pdf(const Vector3f &wo, const Vector3f &wi) const;
-    std::string ToString() const;
+namespace pbrt{
 
-  private:
-
-    Zipin zipin;
-};
-
-struct ZipinHit {
-    ZipinHit(loat thetaR, char side, char bounce, float G): 
+struct Hit {
+    Hit(Float thetaR, char bounce, char side, float G): 
         thetaR(thetaR), G(G), side(side), bounce(bounce) {};
+
+    bool isHit(float thetaI, char bcount, char s) {
+        return fabs(thetaR-thetaI) < 1e-4 && bounce == bcount && side == s;
+    }
     
     Float thetaR, G;
-    char side;
     char bounce;
-    Float G;
+    char side;
     //xmin, xmax, ratio are only used for debugging 
 };
 
 
-Float stableCeiling(Float psi, Float gAngle) {
+inline Float stableCeiling(Float psi, Float gAngle) {
     int k = int(psi/gAngle);
-    if psi - k * gAngle < 1e-6 return k
+    if (psi - k * gAngle < 1e-3) return k;
     return k + 1;
 }
 
 class VGroove {
   public:
-    std::vector<Hit> Eval(Float thetaG, Float thetaO); 
-    std::vector<Hit> nearEval(Float thetaG, Float thetaO);
-    std::vector<Hit> farEval(Float thetaG, Float thetaO);
+    inline std::vector<Hit>& eval(float thetaG, float thetaO); 
+    inline void farEval(float theta, float phi, float maxX, bool hasNear, float hitrange);
+    inline float nearEval(float theta, float phi, float hitrange);
+    inline void addHit(float xi, int bcount, char side, float G);
+    inline bool inverseEval(float thetaO, float thetaI, char bounceCount, char side, float &thetaM, float& G);
+  private:
+    inline static bool computeThetaM(float thetaO, float thetaI, int bounceCount, char side, float &thetaM);
+    inline static bool computeRightThetaM(float thetaO, float thetaI, int bounceCount, float& theta);
+    inline static bool computeLeftThetaM(float thetaO, float thetaI, int bounceCount, float& theta);
+    std::vector<Hit> theHits;
 };
 
-void VGroove::addHit(std::vector<Hit>& hits, float xi, char bcount, 
-    char side, float G) {
+bool 
+VGroove::computeRightThetaM(float thetaO, float thetaI, int n, float &theta) {
+    float xchi = -thetaI;
+    if ((n+1)%2 == 1) xchi *= -1;
+    theta = (Pi + thetaO - xchi) *.5/n;
+    if (theta > thetaO and theta < .5 * Pi) return true;
+    return false; 
+}
+
+bool
+VGroove::computeLeftThetaM(float thetaO, float thetaI, int n, float& theta) {
+    float xchi = -thetaI;
+    if (n%2 == 1) xchi *= -1;
+    theta = (Pi - thetaO - xchi) *.5/n;
+    if (theta > 1e-6 && theta < .5 * Pi) return true;
+    return false;
+} 
+
+bool
+VGroove::computeThetaM(float thetaO, float thetaI, int bounceCount, char side, float& thetaM) {
+    if (side == 'l')  {
+        return computeLeftThetaM(thetaO, thetaI, bounceCount, thetaM);
+    } else {
+        return computeRightThetaM(thetaO, thetaI, bounceCount, thetaM);
+    }
+}
+
+bool
+VGroove::inverseEval(float thetaO, float thetaI, char bounceCount, char side, float &G, float &thetaM) {
+
+    G = 0;
+    if (thetaO < 0) {
+        thetaO = -thetaO;
+        thetaI = -thetaI;
+        //print('zipin expects thetaO to be positive')
+    }
+
+    if (thetaO + .0001 > .5 * Pi) return false; 
+    bool validGroove = computeThetaM(thetaO, thetaI, (int) bounceCount, side, thetaM);
+    if (validGroove) {
+        eval(thetaM, thetaO);
+        for (int i = 0; i < theHits.size(); i++) {
+            if (theHits[i].isHit(thetaI, bounceCount, side)) {
+                G = theHits[i].G;
+                return true; 
+            }
+        }
+    }
+    return false; 
+}
+
+void 
+VGroove::addHit(float xi, int bcount, char side, float G) {
+    
+    if (G > 1e-5) theHits.push_back( Hit(-xi, bcount, side, G));
 
     //scale minInterval and maxInterval to [-1, 1] to match pat's ray tracer
     //float minInterval = interval[0]/range * 2.0;
     //float maxInterval = interval[1]/range * 2.0;
     //zipin ratio is still within [0, 1] range
-
-    float r = (interval[1] - interval[0])/range;
-    if (G > 1e-5) hits.push_back( ZipinHit(-xi, s, bcount, G));
+    //float r = (interval[1] - interval[0])/range;
 } 
 
-void VGroove::far(std::vector<Hit>& hits, float theta, float phi, float maxX, bool hasNear, 
-        float hitrange):
+void 
+VGroove::farEval(float theta, float phi, float maxX, bool hasNear, float hitrange) {
 
     float gAngle = 2.0 * theta;
     float psi_min = Pi - 2.0 *(theta + phi);
 
     float sinThetaPhi = sin(theta+phi);
 
-    float psi_max = Pi - (theta+phi)
+    float psi_max = Pi - (theta+phi);
 
     float zMax = 1;
 
-    if (hasNear == False && sinThetaPhi > 0) {
+    if (hasNear == false && sinThetaPhi > 0) {
         //this is exactly Gi because when all the rays go to the left side
         //the max left side visible is Gi
         //equation 9 in Zipin Paper, hitrange is 2sinTheta
 
-        zMax = hitrange * cos(phi)/ sinThetaPhi 
-        psi_max -= sin((1.0-zMax) * sinThetaPhi)
+        zMax = hitrange * cos(phi)/ sinThetaPhi;
+        psi_max -= sin((1.0-zMax) * sinThetaPhi);
     } 
 
     //print(mt.degrees(psi_min), mt.degrees(psi_max))
 
-    int n_min= stableCeiling(psi_min, gAngle)
-    int n_max = stableCeiling(psi_max, gAngle)
+    int n_min= stableCeiling(psi_min, gAngle);
+    int n_max = stableCeiling(psi_max, gAngle);
     
     //if n_max - n_min > 1:
     //    print(theta, phi, n_max, n_min) 
 
-    float xi_min = Pi - phi - n_min * gAngle
-    float xi_max = Pi - phi - n_max * gAngle
+    float xi_min = Pi - phi - n_min * gAngle;
+    float xi_max = Pi - phi - n_max * gAngle;
     
-    float x_min_intersect = -hitrange * .5
+    float x_min_intersect = -hitrange * .5;
     float x_max_intersect = maxX;
     float xrange = maxX - x_min_intersect;
 
@@ -96,28 +142,26 @@ void VGroove::far(std::vector<Hit>& hits, float theta, float phi, float maxX, bo
     if (n_max > n_min) {
         //compute z critical intersect, length from circle center
         int k = n_min;
-        criticalAngle = Pi - (theta+phi) - k* gAngle;
-        if (criticalAngle <= 0) {
-            z = 0;
-        } else {
+        float criticalAngle = Pi - (theta+phi) - k* gAngle;
+        float z = 0;
+        if (criticalAngle > 0) {
             z = sin(criticalAngle)/sinThetaPhi;
         }
+        addHit(xi_max, n_max, 'l', zMax - (1-z));
+        addHit(xi_min, n_min, 'l', 1-z);
     
-        float x_critical_intersect = x_min_intersect + (1-z) / zMax  * xrange
-
+        //float x_critical_intersect = x_min_intersect + (1-z) / zMax  * xrange
         //addHit(hits, xi_max, n_max, 'l', zMax - (1-z), (x_critical_intersect, x_max_intersect), hitrange);
         //addHit(hits, xi_min, n_min, 'l', 1-z, (x_min_intersect, x_critical_intersect), hitrange);
-        addHit(hits, xi_max, n_max, 'l', zMax - (1-z));
-        addHit(hits, xi_min, n_min, 'l', 1-z);
     } else {
+        addHit(xi_min, n_min, 'l', zMax);
+
         //addHit(hits, xi_min, n_min, 'l', zMax, (x_min_intersect, x_max_intersect), hitrange);
-        addHit(hits, xi_min, n_min, 'l', zMax);
     }
-   
-    return hits;
 }
 
-void VGroove:: near(std::vector<Hits> hits, float theta, float phi, float hitrange) {
+float
+VGroove::nearEval(float theta, float phi, float hitrange) {
 
     float gAngle = 2.0 * theta;
    
@@ -129,10 +173,11 @@ void VGroove:: near(std::vector<Hits> hits, float theta, float phi, float hitran
     //psi_max = mt.pi - 2.0 * phi
     //psi_min = mt.pi - theta - phi
 
-    float x_near_min = os(theta) * tan(phi);
+    float x_near_min = cos(theta) * tan(phi);
 
     int n_min = stableCeiling(psi_min, gAngle);
     int n_max = stableCeiling(psi_max, gAngle);
+
     //if (n_max - n_min) > 1:
     //    print(theta, phi, n_max, n_min) 
 
@@ -145,32 +190,63 @@ void VGroove:: near(std::vector<Hits> hits, float theta, float phi, float hitran
     float x_min_intersect = x_near_min;
     float x_max_intersect = sin(theta);
 
-    if (n_min == n_max || theta - phi < .000001) {
-        addHit(hits, xi_min, n_min, 'right', 1.0, (x_min_intersect, x_max_intersect), hitrange)
+    if (n_min == n_max || theta - phi < 1e-5) {
+        //addHit(hits, xi_min, n_min, 'right', 1.0, (x_min_intersect, x_max_intersect), hitrange)
+        addHit(xi_min, n_min, 'r', 1.0);
     } else {
-        k = n_min
-        criticalAngle = mt.pi - (theta-phi) - k * gAngle
-        if criticalAngle <= 0:
-            z = 0
-        else:
-            z = mt.sin(criticalAngle) /mt.sin(theta-phi)
-        x_critical_intersect = x_min_intersect + (x_max_intersect - x_min_intersect) * z
-        addHit(hits, xi_min, n_min, 'right', 1.0-z, (x_critical_intersect, x_max_intersect), hitrange)
-        addHit(hits, xi_max, n_max, 'right', z, (x_min_intersect, x_critical_intersect), hitrange)
+        int k = n_min;
+        float criticalAngle = Pi - (theta-phi) -  gAngle * k;
+        float z = 0;
+        
+        if (criticalAngle > 0) {
+            z = sin(criticalAngle) /sin(theta-phi);
+        }
+        addHit(xi_min, n_min, 'r', 1.0-z);
+        addHit(xi_max, n_max, 'r', z);
+        //x_critical_intersect = x_min_intersect + (x_max_intersect - x_min_intersect) * z;
+        //addHit(hits, xi_min, n_min, 'right', 1.0-z, (x_critical_intersect, x_max_intersect), hitrange)
+        //addHit(hits, xi_max, n_max, 'right', z, (x_min_intersect, x_critical_intersect), hitrange)
     }
+    return x_near_min;
+}
 
-def zipin(theta, phi):
+std::vector<Hit> & 
+VGroove::eval(float thetaR, float phiR) {
 
-    //thetaR = mt.radians(theta)
-    //phiR = mt.radians(phi)
-    thetaR = theta
-    phiR = phi
+    theHits.clear();
+    float xmax = sin(thetaR);
 
-    xmax = mt.sin(thetaR)
+    if (phiR > thetaR) {
+        farEval(thetaR, phiR, sin(thetaR), false, xmax*2);
+    } else {
+        float  x_near_min = nearEval(thetaR, phiR, xmax*2); 
+        farEval(thetaR, phiR, x_near_min, true,  xmax*2);
+    }
+    return theHits;
+}
 
-    if phiR > thetaR:
-        return far(thetaR, phiR, mt.sin(thetaR), False, xmax*2)
-    else:
-        (nearhits, x_near_min) = near(thetaR, phiR, xmax*2) 
-        farhits = far(thetaR, phiR, x_near_min, True,  xmax*2)
-    return farhits + nearhits 
+struct EvalFrame;
+class VGrooveReflection : public MicrofacetReflection {
+  public:
+    // MicrofacetReflection Public Methods
+    VGrooveReflection(const Spectrum &R,
+                         MicrofacetDistribution *distribution, Fresnel *fresnel)
+        : MicrofacetReflection(R, distribution, fresnel) {}
+    Spectrum f(const Vector3f &wo, const Vector3f &wi) const;
+    
+    Spectrum Sample_f(const Vector3f &wo, Vector3f *wi, const Point2f &u,
+                      Float *pdf, BxDFType *sampledType) const;
+    Float Pdf(const Vector3f &wo, const Vector3f &wi) const;
+    //std::string ToString() const;
+
+  private:
+    float microfacetReflectionWithoutG(const Vector3f& wo, const Vector3f& wi,
+                   const Vector3f& wh) const;
+    float microfacetPdf(const Vector3f& wo, const Vector3f& wh) const;
+    float computeBounceBrdf(const EvalFrame& evalFrame, VGroove& vgroove, int bounce, char side,
+                    float& pdf) const;
+    Spectrum eval(const Vector3f &wo, const Vector3f &wi, int maxBounce = 3, int minBounce = 1, float& pdf) const;
+    //VGroove theGroove;
+};
+
+}//end of namespace pbrt
